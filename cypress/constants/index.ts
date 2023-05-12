a3f08e02330d9de64410029233adcb900e9d5fea
kwanso-zubair <zubair.haider@kwanso.com>
2023-05-12 16:40:29 +0500

    Completed Interacting with radio Buttons and CheckBoxes

--- cypress/constants/index.ts
+++ cypress/constants/index.ts
@@ -1,2 +1,4 @@
 export const url = 'https://app.collegeadvisor.com/login';
-export const radioButtonUrl = 'https://materializecss.com/radio-buttons.html';
+export const radioButtonUrl =
+  'https://demos.jquerymobile.com/1.4.5/checkboxradio-radio/';
+export const checkBoxesUrl = `https://demos.jquerymobile.com/1.4.5/checkboxradio-checkbox/`;
--- cypress/e2e/integrations/checkboxesAndRadio.cy.ts
+++ cypress/e2e/integrations/checkboxesAndRadio.cy.ts
@@ -1,15 +1,32 @@
-import { radioButtonUrl } from '../../constants';
+import { checkBoxesUrl, radioButtonUrl } from '../../constants';
 
 describe('Radio Button & CheckBoxes', () => {
   it('Radio Buttons', () => {
-    cy.visit(radioButtonUrl);
-    cy.xpath(`//form[@action='#']//span[contains(text(),'Red')]`).should(
-      'be.visible'
-    );
-    for (let radioButton = 1; radioButton < 5; radioButton++) {
-      cy.get(
-        `:nth-child(${radioButton}) > label > input[name='group1']`
-      ).should(radioButton === 1 ? `be.checked` : `not.be.checked`);
-    }
+    cy.visit(radioButtonUrl); // 'https://demos.jquerymobile.com/1.4.5/checkboxradio-radio/';
+    cy.get(`:nth-child(6) > form > :nth-child(1) > .ui-btn`).click();
+    cy.get(`input#radio-choice-0a`).should('be.checked');
+    cy.get(`input#radio-choice-0b`).should('not.be.checked');
+  });
+  it('CheckBoxes', () => {
+    cy.visit(checkBoxesUrl);
+    cy.get(
+      ':nth-child(16) > form > .ui-controlgroup > .ui-controlgroup-controls > :nth-child(1) > .ui-btn'
+    ).click();
+    cy.get('input#checkbox-v-2a').should('be.checked');
+    cy.get('input#checkbox-v-2b').should('not.be.checked');
+
+    cy.get(
+      ':nth-child(16) > form > .ui-controlgroup > .ui-controlgroup-controls > :nth-child(n) > .ui-btn'
+    ).click({ multiple: true });
+    cy.get('input#checkbox-v-2a').should('not.be.checked');
+    cy.get('input#checkbox-v-2b').should('be.checked');
+    cy.get('input#checkbox-v-2c').should('be.checked');
+
+    cy.get(
+      ':nth-child(16) > form > .ui-controlgroup > .ui-controlgroup-controls > :nth-child(n) > .ui-btn'
+    ).click({ multiple: true });
+    cy.get('input#checkbox-v-2a').should('be.checked');
+    cy.get('input#checkbox-v-2b').should('not.be.checked');
+    cy.get('input#checkbox-v-2c').should('not.be.checked');
   });
 });
